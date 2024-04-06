05402fe3b49511308144d57030241a5cbdfca713
muhammadkashif20 <kashif28@gmail.com>
2024-04-06 07:55:49 -0700

    problem solve

--- app.js
+++ app.js
@@ -19,16 +19,10 @@ document.write(Data_type)
 // Question 04 :- :
 document.write("<br>")
 document.write("<br>")
-document.write("<br>")
-document.write("<br>")
-document.write("<br>")
-var Visitors_name="John Doe "
-document.write(Visitors_name)
 
+var Visitorname='John Doe'
+var Producttitle='T Shirt(s)'
+var Quantity=5
 
-var Product_title="ordered"
-document.write(Product_title)
-var Quantity=" 5 T-shirt(s) "
-document.write(Quantity)
-var shop=('on XYZ Clothing store.')
-document.write(shop)
+var order=Visitorname +' ordered ' + Quantity+Producttitle +' on XYZ Clothing store'
+document.write(order)
